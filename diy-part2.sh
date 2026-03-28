08be6b7edefa938c2a8d8afb3861199202471528
xxjeanxx80 <87995010+xxjeanxx80@users.noreply.github.com>
2026-03-29 03:11:14 +0700

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -1,17 +1,5 @@
-#!/bin/bash
-#
-# Copyright (c) 2019-2020 P3TERX <https://p3terx.com>
-#
-# This is free software, licensed under the MIT License.
-# See /LICENSE for more information.
-#
-# https://github.com/P3TERX/Actions-OpenWrt
-# File name: diy-part2.sh
-# Description: OpenWrt DIY script part 2 (After Update feeds)
-#
-# Xoa wpad co ban
-# 1. Tải mã nguồn luci-app-easymesh từ kho lưu trữ (Link sống 100%)
-git clone https://github.com/ntlf9t/luci-app-easymesh.git package/luci-app-easymesh
+# 1. Tải mã nguồn luci-app-easymesh từ kho chuẩn của TorGuard
+git clone https://github.com/torguardvpn/luci-app-easymesh.git package/luci-app-easymesh
 
 # 2. Xóa các bản wpad cắt giảm (không hỗ trợ bảo mật Mesh)
 sed -i 's/CONFIG_PACKAGE_wpad-basic-mbedtls=y/# CONFIG_PACKAGE_wpad-basic-mbedtls is not set/g' .config
