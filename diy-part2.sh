fbe2f050bb2169663b2eedd3b61fc9366a2ccf0a
xxjeanxx80 <87995010+xxjeanxx80@users.noreply.github.com>
2026-03-29 03:07:38 +0700

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -10,13 +10,17 @@
 # Description: OpenWrt DIY script part 2 (After Update feeds)
 #
 # Xoa wpad co ban
+# 1. Tải mã nguồn luci-app-easymesh từ kho lưu trữ (Link sống 100%)
+git clone https://github.com/ntlf9t/luci-app-easymesh.git package/luci-app-easymesh
+
+# 2. Xóa các bản wpad cắt giảm (không hỗ trợ bảo mật Mesh)
 sed -i 's/CONFIG_PACKAGE_wpad-basic-mbedtls=y/# CONFIG_PACKAGE_wpad-basic-mbedtls is not set/g' .config
 sed -i 's/CONFIG_PACKAGE_wpad-basic=y/# CONFIG_PACKAGE_wpad-basic is not set/g' .config
 sed -i 's/CONFIG_PACKAGE_wpad-mini=y/# CONFIG_PACKAGE_wpad-mini is not set/g' .config
 
-# Them wpad xịn va EasyMesh
+# 3. Kích hoạt wpad xịn và gọi các lõi EasyMesh vào công thức nấu ROM
 echo "CONFIG_PACKAGE_wpad-openssl=y" >> .config
-echo "CONFIG_PACKAGE_luci-app-easymesh=y" >> .config
 echo "CONFIG_PACKAGE_ieee1905=y" >> .config
 echo "CONFIG_PACKAGE_map-agent=y" >> .config
 echo "CONFIG_PACKAGE_map-controller=y" >> .config
+echo "CONFIG_PACKAGE_luci-app-easymesh=y" >> .config
