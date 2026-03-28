c0c7afbfdff3b3d4c23295844b799c4b041ac51b
xxjeanxx80 <87995010+xxjeanxx80@users.noreply.github.com>
2026-03-29 02:56:48 +0700

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -9,3 +9,14 @@
 # File name: diy-part2.sh
 # Description: OpenWrt DIY script part 2 (After Update feeds)
 #
+# Xoa wpad co ban
+sed -i 's/CONFIG_PACKAGE_wpad-basic-mbedtls=y/# CONFIG_PACKAGE_wpad-basic-mbedtls is not set/g' .config
+sed -i 's/CONFIG_PACKAGE_wpad-basic=y/# CONFIG_PACKAGE_wpad-basic is not set/g' .config
+sed -i 's/CONFIG_PACKAGE_wpad-mini=y/# CONFIG_PACKAGE_wpad-mini is not set/g' .config
+
+# Them wpad xịn va EasyMesh
+echo "CONFIG_PACKAGE_wpad-openssl=y" >> .config
+echo "CONFIG_PACKAGE_luci-app-easymesh=y" >> .config
+echo "CONFIG_PACKAGE_ieee1905=y" >> .config
+echo "CONFIG_PACKAGE_map-agent=y" >> .config
+echo "CONFIG_PACKAGE_map-controller=y" >> .config
